b1e7f418947873103c55eb4675afbfb4276c3872
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-09-09 01:12:52 -0500

    fix: #size added

--- linked_list.rb
+++ linked_list.rb
@@ -8,8 +8,6 @@ class Node
 end
 
 class LinkedList
-  attr_accessor :size, :head, :tail
-
   def initialize
     @head = nil
     @tail = nil
@@ -17,16 +15,27 @@ class LinkedList
 
   def append(value)
     new_node = Node.new(value)
-    tail.next_node = new_node if tail
-    self.head = new_node unless head
-    self.tail = new_node
+    @tail.next_node = new_node if @tail
+    @head = new_node unless @head
+    @tail = new_node
   end
 
   def prepend(value)
     new_node = Node.new(value)
-    new_node.next_node = head
-    self.head = new_node
-    self.tail = new_node unless tail
+    new_node.next_node = @head
+    @head = new_node
+    @tail = new_node unless @tail
+  end
+
+  def size
+    count = 0
+    current_node = @head
+    loop do
+      break if current_node == nil
+      count += 1
+      current_node = current_node.next_node
+    end
+    count
   end
 end
 
@@ -34,4 +43,4 @@ new_list = LinkedList.new
 new_list.prepend(3)
 new_list.prepend(2)
 new_list.prepend(1)
-p new_list
+p new_list.size

5b4374a7b51c0b53e603294f99eb59760551ad44
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-09-09 00:44:08 -0500

    fix: #append created

--- linked_list.rb
+++ linked_list.rb
@@ -1,16 +1,30 @@
 class Node
-  def initialize(value = nil)
-    attr_accessor :next_node, :value
+  attr_accessor :value, :next_node
 
+  def initialize(value = nil)
     @value = value
     @next_node = nil
   end
 end
 
 class LinkedList
+  attr_accessor :size, :head, :tail
+
   def initialize
     @size = 0
     @head = nil
     @tail = nil
   end
+
+  def append(value)
+    new_node = Node.new(value)
+    tail.next_node = new_node if tail
+    self.head = new_node unless head
+    self.tail = new_node
+  end
 end
+
+new_list = LinkedList.new
+p new_list
+new_list.append(3)
+p new_list
